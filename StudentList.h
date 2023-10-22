c0afcbfa9f830b4baf1339c013ca15ae6967baa4
cristheogat24 <acsanchez1123@gmail.com>
2023-10-21 20:43:14 -0500

    first go ahead before checking in

--- StudentList.h
+++ StudentList.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <iostream>
 #include <string>
 using namespace std;
@@ -39,8 +40,8 @@ private:
 	{
 	public:
 		student data;
-		node * next;
-		node * prev;
+		node* next;
+		node* prev;
 
 		node(student x)
 		{
@@ -50,40 +51,196 @@ private:
 		}
 	};
 
-	node * head;
-	node * tail;
+	node* head;
+	node* tail;
 
 public:
-	studentList();
+	studentList()
+	{
+		head = nullptr;
+		tail = nullptr;
+	}
 
 	//add a student to the list.
 	//Must run in O(1) time.
-	void insert(student s);
+	void insert(student s)
+	{
+		node* person = new node(s);
+		if (head == NULL)
+		{
+			head = person;
+			tail = person;
+		}
+		else
+		{
+			tail->next = person;
+			person->prev = tail;
+			tail = person;
+		}
+	}
 
 	//find the student with the given id number and return it
 	//What is the worst case run time of this?
 	//What is the average case run time of this?
-	student retrieveStudent(unsigned int idnumber);
+	student retrieveStudent(unsigned int idnumber)
+	{
+		node* current = head;
+		while (current != NULL)
+		{
+			if (current->data.id == idnumber)
+			{
+				return current->data;
+			}
+			current = current->next;
+
+		}
+		//just in case theres a mistake when putting a student information wrong
+		return student();
+	}
 
 	//Change the gpa of the student with given id number to newGPA
 	//What is the run time?
-	void updateGPA(unsigned int idnumber, double newGPA);
+	void updateGPA(unsigned int idnumber, double newGPA)
+	{
+		node* current = head;
+		while (current != NULL)
+		{
+			if (current->data.id == idnumber)
+			{
+				current->data.gpa = newGPA;
+				break;
+			}
+			current = current->next;
+		}
+
+	}
 
 	//Add all students from otherlist to this list.
 	//otherlist should be empty after this operation.
 	//Must run in O(1) time.
-	void mergeList(studentList &otherlist);
+	void mergeList(studentList& otherlist)
+	{
+		
+		if (otherlist.head == NULL)
+		{
+			return;
+		}
+
+		if (head == NULL)
+		{
+			head = otherlist.head;
+			tail = otherlist.tail;
+		}
+		else
+		{
+			tail->next = otherlist.head;
+			otherlist.head->prev = tail;
+			tail = otherlist.tail;
+		}
+
+		otherlist.head = NULL;
+		otherlist.tail = NULL;
+	}
 
 	//create a list of students whose gpa is at least minGPA.
 	//Return this list.  The original list should
 	//not be modified (do not remove the students from the original list).
 	//Run time?
-	studentList honorRoll(double minGPA);
+	studentList honorRoll(double minGPA)
+	{
+		studentList honorlist;
+		node* current = head;
+		while (current != NULL)
+		{
+			if (current->data.gpa >= minGPA)
+			{
+				honorlist.insert(current->data);
+			}
+			current = current->next;
+		}
+		return honorlist;
+	}
 
 	//sort the list by the given field ("name", "id", or "gpa").
-	//Run time?
-	void sort(string field);
+	//Run time? 
+	void sort(string field)
+	{
+		if (field == "name")
+		{
+			node* curr, * index;
+			student temp;
+			if (head == NULL)
+			{
+				return;
+			}
+			for (curr = head; curr->next != NULL; curr = curr->next)
+			{
+				for (index = curr->next; index != NULL; index = index->next)
+				{
+					if (curr->data.name > index->data.name)
+					{
+						temp = curr->data;
+						curr->data = index->data;
+						index->data = temp;
+					}
+				}
+			}
+		}
+		else if (field == "id")
+		{
+			node* curr, * index;
+			student temp;
+			if (head == NULL)
+			{
+				return;
+			}
+			for (curr = head; curr->next != NULL; curr = curr->next)
+			{
+				for (index = curr->next; index != NULL; index = index->next)
+				{
+					if (curr->data.id > index->data.id)
+					{
+						temp = curr->data;
+						curr->data = index->data;
+						index->data = temp;
+					}
+				}
+			}
+		}
+		else if (field == "gpa")
+		{
+			node* curr, * index;
+			student temp;
+			if (head == NULL)
+			{
+				return;
+			}
+			for (curr = head; curr->next != NULL; curr = curr->next)
+			{
+				for (index = curr->next; index != NULL; index = index->next)
+				{
+					if (curr->data.gpa > index->data.gpa)
+					{
+						temp = curr->data;
+						curr->data = index->data;
+						index->data = temp;
+					}
+				}
+			}
+		}
+	}
 
 	//Print out each student in the list.  This is mainly for testing purposes.
-	void printList();
+	void printList()
+	{
+		node* list = head;
+		while (list != NULL)
+		{
+			cout << list->data.name << " ";
+			cout << list->data.id << " ";
+			cout << list->data.gpa << endl;
+
+			list = list->next;
+		}
+	}
 };
---
+++ test.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<string>
+#include "StudentList.h"
+using namespace std;
+
+int main()
+{
+    studentList roster;
+    studentList roster2;
+
+    student Abel("Abel", 20490457, 2.33);
+    student Oscar("Oscar", 20484349, 2.12);
+    student Adrian("Adrian", 20413303, 2.86);
+
+
+    roster.insert(Abel);
+    roster.insert(Oscar);
+    roster.insert(Adrian);
+
+    roster.printList();
+    cout << endl;
+    
+    student Cristian("Cristian", 19198542, 1.98);
+    student Debbie("Debbie", 78531421, 4.0);
+    student Amber("Amber", 88551133, 2.99);
+
+    roster2.insert(Cristian);
+    roster2.insert(Debbie);
+    roster2.insert(Amber);
+
+    roster2.printList();
+    cout << endl;
+
+    student retrievedStudent = roster.retrieveStudent(20484349);
+    cout << "Name: " << retrievedStudent.name << ", ID: " << retrievedStudent.id << ", GPA: " << retrievedStudent.gpa << endl;
+    cout << endl;
+
+    roster.updateGPA(20490457, 4.0);
+    roster.updateGPA(20484349, 3.87);
+    roster.updateGPA(20413303, 3.90);
+    roster.printList();
+    cout << endl;
+
+    studentList honorList = roster.honorRoll(3.5);
+    honorList.printList();
+    cout << endl;
+
+    //merge list 2 into list 1
+    roster.mergeList(roster2);
+    cout << "after merging list 2 into list 1: " << endl;
+    roster.printList();
+    cout << endl;
+    cout << "list 2 after merging into list 1: " << endl;
+    roster2.printList();
+
+    return 0;
+
+}
